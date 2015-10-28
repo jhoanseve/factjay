e016f4aeace00195332b19f56f0a024aafb1a874
Joan Sepulveda <jhoanseve6@gmail.com>
2015-10-27 20:09:49 -0500

    Commit init project

---
+++ src/main/java/com/wengi/FactjayApplication.java
@@ -0,0 +1,12 @@
+package com.wengi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactjayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FactjayApplication.class, args);
+    }
+}
